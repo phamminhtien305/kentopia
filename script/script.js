16b4dfad1a405467d9c3f0f2090d5da763674336
Fra-TienPM <phamtien@Fra-TienPMs-MacBook-Pro.local>
2017-12-07 13:59:36 +0700

    Update script

---
+++ script/script.js
@@ -0,0 +1,19 @@
+window.onload = onPageLoad('state');
+
+function onPageLoad(sParam) {
+  
+  	var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split("&");
+
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split("=");
+        if (sParameterName[0] == sParam) {
+        	if (sParameterName[1] == "did_navi") {
+        		document.getElementById("start_navi_").disabled = true; 	
+        	} else {
+        		document.getElementById("start_route_").disabled = true; 	
+        	}
+        	document.getElementById("txt_description_").innerHTML = "Nhu lin"; 	
+        }
+    }
+}
