d6f246b8bb84851aa678b5444294f0d94a6aeca4
Fra-TienPM <phamtien@Fra-TienPMs-MacBook-Pro.local>
2017-12-15 13:33:46 +0700

    Update scrip

---
+++ script/script.js
@@ -0,0 +1,62 @@
+window.onload = onPageLoad('state');
+
+function onPageLoad(sParam) {
+  
+  	var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split("&");
+
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split("=");
+        if (sParameterName[0] == sParam) {
+        	if (sParameterName[1] == "did_navi") {
+        		document.getElementById("txt_description_").innerHTML = "案内終了";
+        	} else {
+        		document.getElementById("txt_description_").innerHTML = "終了";
+        	}
+        	document.getElementById("start_package_id_").style.display = 'none';
+        	document.getElementById("start_route_").style.display = 'none';
+        	document.getElementById("start_point_").style.display = 'none';
+        }
+    }
+
+	if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+    {
+    	var intent= document.getElementById("start_package_id_").href;
+    	intent = intent.replace('BROWSER','Opera');
+    	document.getElementById("start_package_id_").href = intent
+
+    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 )
+    {
+        var intent= document.getElementById("start_package_id_").href;
+    	intent = intent.replace('BROWSER','Chrome');
+    	document.getElementById("start_package_id_").href = intent
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1)
+    {
+    	var intent= document.getElementById("start_package_id_").href;
+    	intent = intent.replace('BROWSER','Safari');
+    	document.getElementById("start_package_id_").href = intent
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+    {
+         var intent= document.getElementById("start_package_id_").href;
+    	intent = intent.replace('BROWSER','Firefox');
+    	document.getElementById("start_package_id_").href = intent
+    }
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    {
+     	var intent= document.getElementById("start_package_id_").href;
+    	intent = intent.replace('BROWSER','IE');
+    	document.getElementById("start_package_id_").href = intent
+    }  
+    else 
+    {
+    	var intent= document.getElementById("start_package_id_").href;
+    	intent = intent.replace('BROWSER','unknown');
+    	document.getElementById("start_package_id_").href = intent
+    }
+ 
+    
+
+}
