641fecd22bacc0117edc41ce293d99f97b7eb45d
Fra-TienPM <phamtien@Fra-TienPMs-MacBook-Pro.local>
2017-12-26 08:20:42 +0700

    Update Page

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 window.onload = onPageLoad('state');
 
 function onPageLoad(sParam) {
-    document.getElementById("status").innerHTML = "Chrome"
+    document.getElementById("status").innerHTML = "Chrome";
   	var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split("&");
 
@@ -26,42 +26,40 @@ function onPageLoad(sParam) {
     	intent = intent.replace("BROWSER","Opera");
     	document.getElementById("start_navi_").href = intent;
     	document.getElementById("status").innerHTML = "Opera";
-    } else {
-        document.getElementById("status").innerHTML = navigator.userAgent;
     }
- //    else if(navigator.userAgent.indexOf("Chrome") != -1 )
- //    {
- //        var intent= document.getElementById("start_navi_").href;
- //    	intent = intent.replace("BROWSER","Chrome");
- //    	document.getElementById("start_navi_").href = intent
- //    	document.getElementById("status").innerHTML = "Chrome"
- //    }
- //    else if(navigator.userAgent.indexOf("Safari") != -1)
- //    {
- //    	var intent= document.getElementById("start_navi_").href;
- //    	intent = intent.replace("BROWSER","Safari");
- //    	document.getElementById("start_navi_").href = intent
- //    	document.getElementById("status").innerHTML = "Safari"
- //    }
- //    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
- //    {
- //         var intent= document.getElementById("start_navi_").href;
- //    	intent = intent.replace("BROWSER","Firefox");
- //    	document.getElementById("start_navi_").href = intent
- //    	document.getElementById("status").innerHTML = "Firefox"
- //    }
- //    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
- //    {
- //     	var intent= document.getElementById("start_navi_").href;
- //    	intent = intent.replace("BROWSER","IE");
- //    	document.getElementById("start_navi_").href = intent
- //    	document.getElementById("status").innerHTML = "IE"
- //    }  
- //    else 
- //    {
- //    	var intent= document.getElementById("start_navi_").href;
- //    	intent = intent.replace("BROWSER","unknown");
- //    	document.getElementById("start_navi_").href = intent
- //    	document.getElementById("status").innerHTML = "unknown"
- //    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 )
+    {
+        var intent= document.getElementById("start_navi_").href;
+    	intent = intent.replace("BROWSER","Chrome");
+    	document.getElementById("start_navi_").href = intent;
+    	document.getElementById("status").innerHTML = "Chrome";
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1)
+    {
+    	var intent= document.getElementById("start_navi_").href;
+    	intent = intent.replace("BROWSER","Safari");
+    	document.getElementById("start_navi_").href = intent;
+    	document.getElementById("status").innerHTML = "Safari";
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+    {
+         var intent= document.getElementById("start_navi_").href;
+    	intent = intent.replace("BROWSER","Firefox");
+    	document.getElementById("start_navi_").href = intent;
+    	document.getElementById("status").innerHTML = "Firefox";
+    }
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    {
+     	var intent= document.getElementById("start_navi_").href;
+    	intent = intent.replace("BROWSER","IE");
+    	document.getElementById("start_navi_").href = intent;
+    	document.getElementById("status").innerHTML = "IE";
+    }  
+    else 
+    {
+    	var intent= document.getElementById("start_navi_").href;
+    	intent = intent.replace("BROWSER","unknown");
+    	document.getElementById("start_navi_").href = intent;
+    	document.getElementById("status").innerHTML = "unknown";
+    }
 }
