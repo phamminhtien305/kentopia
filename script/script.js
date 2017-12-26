3fabdfca978f9150793f1bfe7081775c53ee9fc9
Fra-TienPM <phamtien@Fra-TienPMs-MacBook-Pro.local>
2017-12-26 07:54:18 +0700

    Update Page

--- script/script.js
+++ script/script.js
@@ -24,7 +24,7 @@ function onPageLoad(sParam) {
     	var intent= document.getElementById("start_navi_").href;
     	intent = intent.replace("BROWSER","Opera");
     	document.getElementById("start_navi_").href = intent
-    	document.getElementById("title").innerHTML = 'Opera'
+    	document.getElementById("title").innerHTML = "Opera"
 
     }
     else if(navigator.userAgent.indexOf("Chrome") != -1 )
@@ -39,27 +39,27 @@ function onPageLoad(sParam) {
     	var intent= document.getElementById("start_navi_").href;
     	intent = intent.replace('BROWSER','Safari');
     	document.getElementById("start_navi_").href = intent
-    	document.getElementById("title").innerHTML = 'Safari'
+    	document.getElementById("title").innerHTML = "Safari"
     }
     else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
     {
          var intent= document.getElementById("start_navi_").href;
     	intent = intent.replace('BROWSER','Firefox');
     	document.getElementById("start_navi_").href = intent
-    	document.getElementById("title").innerHTML = 'Firefox'
+    	document.getElementById("title").innerHTML = "Firefox"
     }
     else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
     {
      	var intent= document.getElementById("start_navi_").href;
     	intent = intent.replace('BROWSER','IE');
     	document.getElementById("start_navi_").href = intent
-    	document.getElementById("title").innerHTML = 'IE'
+    	document.getElementById("title").innerHTML = "IE"
     }  
     else 
     {
     	var intent= document.getElementById("start_navi_").href;
     	intent = intent.replace('BROWSER','unknown');
     	document.getElementById("start_navi_").href = intent
-    	document.getElementById("title").innerHTML = 'unknown'
+    	document.getElementById("title").innerHTML = "unknown"
     }
 }
